9706331b90cfbb4a502d97712c360238aae8bf98
John-Perdix <joao_perdiz@outlook.com>
2024-05-09 17:26:18 +0100

    Liking posts with hashtag

--- instagram-bot.py
+++ instagram-bot.py
@@ -1,8 +1,22 @@
+import datetime
 from instagrapi import Client
-with open("credentioals.txt", "r") as f:
-    username, password = f.reaad.splitlines()
+with open("credenciais.txt", "r") as f:
+    username, password = f.read().splitlines()
 
+# Get today's date
+today = datetime.date.today()
 client = Client()
 client.login(username, password)
 
 hashtag = "parabens"
+
+medias = client.hashtag_medias_recent(hashtag, 20)
+
+# Filter posts taken after today
+posts_after_today = [post for post in medias if post.taken_at.date() >= today]
+
+
+for i, media in enumerate(posts_after_today):
+    client.media_like(media.id)
+    print(f"liked post numer {i+1} of hashtag {hashtag}")
+
