13706391f376a670bb19511d82367de597d1c16d
John-Perdix <joao_perdiz@outlook.com>
2024-05-10 00:15:01 +0100

    Download (Not Working)

---
+++ DM.py
@@ -0,0 +1,20 @@
+from instagrapi import Client
+
+with open("credenciais.txt", "r") as f:
+    username, password = f.read().splitlines()
+
+# Login to Instagram
+client = Client()
+client.login(username, password)
+
+# Recipient's Instagram user_ids and the message you want to send
+recipient_user_ids = client.user_id_from_username("danscottt")  # Add recipient's user_ids here
+message = "Want to see the moonlight?"
+
+# Send direct message
+client.direct_send(text=message, user_ids=recipient_user_ids)
+
+# Logout
+client.logout()
+
+print("Direct message sent successfully!")
--- instagram-bot.py
+++ instagram-bot.py
@@ -10,13 +10,47 @@ client.login(username, password)
 
 hashtag = "parabens"
 
-medias = client.hashtag_medias_recent(hashtag, 20)
+medias = client.hashtag_medias_recent(hashtag, 3)
 
 # Filter posts taken after today
 posts_after_today = [post for post in medias if post.taken_at.date() >= today]
 
 
-for i, media in enumerate(posts_after_today):
+#for i, media in enumerate(medias):
+#    mediaId = media.id
+#    client.media_like(mediaId)
+#    pictureUrl = media.url
+#    pictureData = client.photo_download(pictureUrl)
+#    with open("downloaded_picture.jpg", "wb") as f:
+#        f.write(pictureData)
+#        print("picture downloaded")
+#    #print(f"liked post numer {i+1} of hashtag {hashtag}")
+
+
+for i, media in enumerate(medias):
+    # Like the post
     client.media_like(media.id)
-    print(f"liked post numer {i+1} of hashtag {hashtag}")
+    
+    # Get more information about the media
+    media_info = client.media_info(media.id)
+    
+    # Get the URL of the photo
+    if media_info.media_type == 1:  # 1 represents photo media type
+        picture_url = media_info.urls[0]
+        
+        # Download the photo
+        picture_data = client.photo_download(picture_url)
+        
+        # Save the photo with a unique filename
+        filename = f"insta_{i+1}.jpg"
+        with open(filename, "wb") as f:
+            f.write(picture_data)
+            print(f"Picture downloaded: {filename}")
+    else:
+        print(f"Media {media.id} is not a photo.")
+
+client.logout()
+
+
+client.logout()
 
