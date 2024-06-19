f707117f4553ed1db44bf6ae3cd7996565db184f
Ana2020143716 <anacsrodrigues.46@gmail.com>
2024-06-19 15:33:59 +0100

    faceDetection from videocamera

--- faceDetection.py
+++ faceDetection.py
@@ -1,35 +1,35 @@
-import cv2 
+import cv2
 import numpy as np
 
-face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.sml')
+# Load the pre-trained Haar Cascade for face detection
+face_classifier = cv2.CascadeClassifier('data\haarcascades\haarcascade_frontalface_defaullt.xml')
 
 def detect_faces(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
-    if faces is ():
+    
+    if len(faces) == 0:
         return img
     
-    for(x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    
     return img
 
-cap = cv2.VideoCapture(0)
+# Capture video from the default webcam
+cap = cv2.VideoCapture(1)
 
 while True:
     ret, frame = cap.read()
+    if not ret:
+        print("Failed to capture image")
+        break
+    
     frame = detect_faces(frame)
-
     cv2.imshow('Video Face Detection', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
-cv2.destroyAllWindows()
-# # Load an image using OpenCV
-# img = cv2.imread('firstImage.jpg')
-
-# # Display the image
-# cv2.imshow('Image', img)
-# cv2.waitKey(0)
 cv2.destroyAllWindows()
